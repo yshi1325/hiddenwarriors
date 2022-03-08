a5554a3203cbb0ab5ea585aab95cfb9face0cfbb
Ashley <ashleyshi006@gmail.com>
2022-03-07 22:35:51 -0500

    fisrt commit

---
+++ components/Featured.jsx
@@ -0,0 +1,28 @@
+import styles from "../styles/Featured.module.css";
+import Image from "next/image";
+
+const Featured = () => {
+  const images = [
+    "/img/featured.png",
+    "/img/featured2.png",
+    "/img/featured3.png",
+  ];
+  return (
+    <div className={styles.container}>
+      <div className={styles.arrowContainer}>
+        <Image src="/img/arrowl.png" alt="" layout="fill" />
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.imgContainer}>
+          {images.map((img, i) => (
+            <Image src="/img/featured.png" key={i} alt="" layout="fill" />
+          ))}
+        </div>
+      </div>
+      <div className={styles.arrowContainer}></div>
+      <Image src="/img/arrowr.png" alt="" layout="fill" />
+    </div>
+  );
+};
+
+export default Featured;
